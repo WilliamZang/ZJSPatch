310b3b4e904f4733b0fc2b7e7fddd23937b57b45
臧成威 <zangchengwei@meituan.com>
2016-08-20 23:33:21 +0800

    Initial Commit

---
+++ ZJSPatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJSPatch
+//
+//  Created by ZangChengwei on 16/8/20.
+//  Copyright © 2016年 ZangChengwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
